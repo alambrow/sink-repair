3d323e407648ee8f2f8dfed9e9d7f64429d337c1
alambrow <alexander.lambrow@gmail.com>
2021-05-05 14:12:35 -0500

    first commit, minor changes

---
+++ src/scripts/Requests.js
@@ -0,0 +1,17 @@
+import { getRequests } from "./dataAccess.js"
+
+export const Requests = () => {
+    const requests = getRequests()
+
+    let html = `
+        <ul>
+            ${
+                requests.map(convertRequestToListElement)
+            }
+        </ul>
+    `
+}
+
+const convertRequestToListElement = () => {
+    // define me!
+}
