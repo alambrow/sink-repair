f1872a6563ee9b33a77b1616b8897b2acc59ea86
alambrow <alexander.lambrow@gmail.com>
2021-05-09 15:52:54 -0500

    delete button added to requests list

--- src/scripts/Requests.js
+++ src/scripts/Requests.js
@@ -1,4 +1,4 @@
-import { getRequests } from "./dataAccess.js"
+import { getRequests, deleteRequest } from "./dataAccess.js"
 
 export const Requests = () => {
     const requests = getRequests()
@@ -6,7 +6,7 @@ export const Requests = () => {
     let html = `
         <ul>
             ${
-                requests.map(convertRequestToListElement)
+                requests.map(convertRequestToListElement).join("\n")
             }
         </ul>
     `
@@ -15,6 +15,19 @@ export const Requests = () => {
 
 const convertRequestToListElement = (request) => {
     return `
-        <li>${request.description}</li>
+        <li>
+        <strong>Service Order ${request.id}</strong>, needed by ${request.neededBy} at <em>${request.address} (address)</em>, for <strong>${request.description}</strong> with a max budget of $${request.budget}.
+        <button class="request__delete" id="request--${request.id}">Delete</button>
+        </li>
     `
 }
+
+// Event listener for delete button
+const mainContainer = document.querySelector("#container")
+
+mainContainer.addEventListener("click", click => {
+    if (click.target.id.startsWith("request--")) {
+        const [,requestId] = click.target.id.split("--")
+        deleteRequest(parseInt(requestId))
+    }
+})
